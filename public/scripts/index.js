27fe04f6605eadaf04d45374675531b2e7ab3985
Roger Luiz <rogerluiz010@gmail.com>
2021-01-29 16:20:56 -0300

    javascript added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,76 +1,199 @@
-window.addEventListener('load', () => {
-  const newTransaction = document.querySelector('#new-transaction')
-  const modalOverlay = document.querySelector('.modal-overlay')
-  const buttonCancel = document.querySelector('.cancel')
+const selectElement = element => {
+  return document.querySelector(element)
+}
 
-  const incomeCard = document.querySelector('#incomeCard p')
-  const expenseCard = document.querySelector('#expenseCard p')
-  const totalCard = document.querySelector('#totalCard p')
-  
-  newTransaction.addEventListener('click', () => {
-    modalOverlay.classList.add('active')
-  })
+const newTransaction = selectElement('#new-transaction')
+const buttonCancel = selectElement('.cancel')
+const modalOverlay = selectElement('.modal-overlay')
+const form = selectElement('form')
 
-  buttonCancel.addEventListener('click', () => {
-    modalOverlay.classList.remove('active')
-  })
+newTransaction.addEventListener('click', () => {
+  modalOverlay.classList.add('active')
+})
 
-  const inputDescription = document.querySelector('#description')
-  const inputAmount = document.querySelector('#amount')
-  const inputDate = document.querySelector('#date')
-  const buttonSave = document.querySelector('#save')
-  const tbody = document.querySelector('#data-table tbody')
+buttonCancel.addEventListener('click', () => {
+  modalOverlay.classList.remove('active')
+})
 
-  buttonSave.addEventListener('click', () => {
-    const description = String(inputDescription.value)
-    const amount = Number(inputAmount.value)
-    const date = String(inputDate.value)
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
+  },
 
-    const className = amount < 0 ? "expense" : "income"
-    createRow(description, formatCurrency(amount), className, formatDate(date))
-
-    modalOverlay.classList.remove('active')
-  })
-
-  const formatCurrency = currencyValue => { // Formatar moeda
-    return currencyValue.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+  set(transactions) {
+    localStorage.setItem('dev.finances:transactions', JSON.stringify(transactions))
   }
+}
 
-  const formatDate = dateValue => { // Formatar data
-    return new Date(dateValue).toLocaleDateString('pt-br')
-  }
+const Transaction = {
+  all: Storage.get(),
+  add(transaction) {
+    Transaction.all.push(transaction)
+    App.reload()
+  },
 
-  const createRow = (descritpionContent, currencyContent, currencyCalass, dateContent) => {
-    const row = document.createElement('tr')
+  remove(index) {
+    Transaction.all.splice(index, 1)
+    App.reload()
+  },
 
-    const tdDescritpion = document.createElement('td')
-    tdDescritpion.classList.add('descritpion')
-    const textDescritpion = document.createTextNode(descritpionContent)
-    tdDescritpion.appendChild(textDescritpion)
-    row.appendChild(tdDescritpion)
+  incomes() {
+    let income = 0
 
-    const tdCurrency = document.createElement('td')
-    const textIncome = document.createTextNode(currencyContent)
-    tdCurrency.classList.add(currencyCalass)
-
-    tdCurrency.appendChild(textIncome)
-    row.appendChild(tdCurrency)
-
-    const tdDate = document.createElement('td')
-    tdDate.classList.add('date')
-    const textDate = document.createTextNode(dateContent)
-    tdDate.appendChild(textDate)
-    row.appendChild(tdDate)
-
-    const tdImage = document.createElement('td')
-    const image = document.createElement('img')
-    image.src = '../assets/minus.svg'
-    tdImage.appendChild(image)
-    row.appendChild(tdImage)
-    image.addEventListener('click', () => {
-      tbody.removeChild(row)
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount > 0) {
+        income += transaction.amount
+      }
     })
 
-    tbody.appendChild(row)
+    return income
+  },
+  
+  expenses() {
+    let expense = 0
+
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount
+      }
+    })
+
+    return expense
+  },
+  
+  total() {
+    return Transaction.incomes() + Transaction.expenses()
   }
+}
+
+const DOM = {
+  transactionsContainer: selectElement('#data-table tbody'),
+
+  addTransactions(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransactions(transaction, index)
+    tr.dataset.index = index
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+
+  innerHTMLTransactions(transaction, index) {
+    const CssClass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+    <td class="descritpion">${transaction.description}</td>
+    <td class="${CssClass}">${amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td><img onclick="Transaction.remove(${index})" src="/../assets/minus.svg"></td>
+    `
+
+    return html
+  },
+
+  updateBalance() {
+    selectElement('#income-display').innerHTML = Utils.formatCurrency(Transaction.incomes())
+    selectElement('#expense-display').innerHTML = Utils.formatCurrency(Transaction.expenses())
+    selectElement('#total-display').innerHTML = Utils.formatCurrency(Transaction.total())
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ''
+  }
+}
+
+const Utils = {
+  formatAmount(value) {
+    value = Number(value) * 100
+    return value
+  },
+
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+    value = String(value).replace(/\D/g, '')
+    value = Number(value) / 100
+    value = value.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+    return signal + value
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split('-')
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+  },
+}
+
+const Form = {
+  description: selectElement('input#description'),
+  amount: selectElement('input#amount'),
+  date: selectElement('input#date'),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value
+    }
+  },  
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues()
+    
+
+    if (description.trim() === '' || amount.trim() === '' || date.trim() === '') {
+      throw new Error('Por favor preencha todos os campos')
+    }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues()
+
+    amount = Utils.formatAmount(amount)
+    date = Utils.formatDate(date)
+
+    return {
+      description,
+      amount,
+      date
+    }
+  },
+
+  clearFields() {
+    Form.description.value = ''
+    Form.amount.value = ''
+    Form.date.value = ''
+  },
+
+  submit(event) {
+    event.preventDefault()
+    try {
+      Form.validateFields()
+      const transaction = Form.formatValues()
+      Transaction.add(transaction)
+      Form.clearFields()
+      modalOverlay.classList.remove('active')
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+}
+
+form.addEventListener('submit', () => {
+  Form.submit(event)
 })
+
+const App = {
+  start() {
+    Transaction.all.forEach(DOM.addTransactions)
+
+    DOM.updateBalance()
+    Storage.set(Transaction.all)
+  },
+  
+  reload() {
+    DOM.clearTransactions()
+    App.start()
+  }
+}
+
+App.start()
--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,5 +1,4 @@
 const button = document.querySelector('#change-theme')
-const img = document.querySelector('#change-theme img')
 
 button.addEventListener('click', () => {
   document.body.classList.toggle('theme-dark')
