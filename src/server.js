b134159e17716da5464547910579422e2d5f3269
Roger Luiz <rogerluiz010@gmail.com>
2021-02-11 19:30:25 -0300

    updated style

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-// Modules and dependencies
 require('dotenv').config()
 
 const express = require('express')
@@ -8,21 +7,17 @@ const router = express.Router()
 const nunjucks = require('nunjucks')
 const { join } = require('path')
 
-// Port
 const PORT = process.env.PORT || 8080
 
-// Nunjucks configuration
 nunjucks.configure(join(__dirname, '/../public/pages'), {
   express: server,
   noCache: true
 })
 
-// Static files
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static(join(__dirname, '/../public/')))
 server.use(router)
 
-// Routes
 router.get('/', (req, res) => {
   res.render('index.html')
 })
@@ -31,8 +26,10 @@ router.get('*', (req, res) => {
   res.render('page-not-found.html')
 })
 
-// Running server
-server.listen(PORT, (err) => {
-  if (err) { console.log('erro') }
-  console.log(`Server running on localhost:${PORT}`)
+server.listen(PORT, error => {
+  const message = error 
+  ? 'Error when running the server' 
+  : `Server running on http://localhost:${PORT}`
+  
+  return console.log(message)
 })
